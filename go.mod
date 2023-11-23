23ff830b81deab7a3b79c25b44ea6e42b7a1577c
Георгий Кораблёв <jasmanchik@gmail.com>
2023-11-23 21:47:24 +0500

    feat(service): реализация сервисного слоя

--- go.mod
+++ go.mod
@@ -4,13 +4,15 @@ go 1.21
 
 require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/golang-jwt/jwt/v5 v5.1.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
 	github.com/jasmanchik/protos v0.0.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/crypto v0.15.0 // indirect
 	golang.org/x/net v0.14.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
 	google.golang.org/grpc v1.59.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
---
+++ internal/services/auth/auth.go
@@ -0,0 +1,159 @@
+package auth
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"log/slog"
+	"sso/internal/domain/models"
+	"sso/internal/lib/jwt"
+	"sso/internal/storage"
+	"strconv"
+	"time"
+)
+
+type Auth struct {
+	log          *slog.Logger
+	userSaver    UserSaver
+	userProvider UserProvider
+	appProvider  AppProvider
+	tokenTTL     time.Duration
+}
+
+type UserSaver interface {
+	SaveUser(
+		ctx context.Context,
+		email string,
+		passHash []byte,
+	) (uid int64, err error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email string) (models.User, error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
+
+type AppProvider interface {
+	App(ctx context.Context, appID int) (models.App, error)
+}
+
+var (
+	ErrInvalidCredentials = errors.New("invalidCredentials")
+	ErrInvalidAppId       = errors.New("invalid app id")
+	ErrUserExists         = errors.New("user already exists")
+)
+
+// New returns new instance of the Auth service
+func New(
+	log *slog.Logger,
+	saver UserSaver,
+	provider UserProvider,
+	appProvider AppProvider,
+	tokenTTL time.Duration,
+) *Auth {
+	return &Auth{
+		log:          log,
+		userSaver:    saver,
+		userProvider: provider,
+		appProvider:  appProvider,
+		tokenTTL:     tokenTTL,
+	}
+}
+
+func (a *Auth) Login(ctx context.Context, email string, pass string, appID int) (string, error) {
+	const op = "auth.Login"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.String("email", email), //careful with sensitive data!
+		slog.String("appID", strconv.Itoa(appID)),
+	)
+	log.Info("start user login")
+
+	user, err := a.userProvider.User(ctx, email)
+	if err != nil {
+		if errors.Is(err, storage.ErrUserNotFound) {
+			a.log.Warn("user not found", err.Error())
+
+			return "", fmt.Errorf("%s : %w", op, ErrInvalidCredentials)
+		}
+
+		a.log.Error("failed to get user", op, err)
+		return "", fmt.Errorf("%s : %w", op, err)
+	}
+
+	err = bcrypt.CompareHashAndPassword(user.PassHash, []byte(pass))
+	if err != nil {
+		a.log.Error("invalid password", op, err)
+		return "", fmt.Errorf("%s : %w", op, ErrInvalidCredentials)
+	}
+
+	app, err := a.appProvider.App(ctx, appID)
+	if err != nil {
+		a.log.Error("app does not exists", op, err.Error())
+
+		return "", fmt.Errorf("%s : %w", err)
+	}
+	log.Info("user logged in successfully")
+
+	token, err := jwt.NewToken(user, app, a.tokenTTL)
+	if err != nil {
+		a.log.Error("failed to create jwt token", op, err.Error())
+
+		return "", fmt.Errorf("%s : %w", err)
+	}
+
+	return token, nil
+}
+
+func (a *Auth) RegisterNewUser(ctx context.Context, email string, pass string) (int64, error) {
+	const op = "auth.RegisterNewUser"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.String("email", email), //careful with sensitive data!
+	)
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+	if err != nil {
+		log.Error("failed to generate a password hash", err.Error())
+		return 0, fmt.Errorf("%s : %w", op, err)
+	}
+
+	userID, err := a.userSaver.SaveUser(ctx, email, hash)
+	if err != nil {
+		if errors.Is(err, storage.ErrUserExists) {
+			a.log.Warn("user not found", err.Error())
+			return 0, fmt.Errorf("%s : %w", op, ErrUserExists)
+		}
+		log.Error("failed to save user", err.Error())
+		return 0, fmt.Errorf("%s : %w", op, err)
+	}
+
+	return userID, nil
+}
+
+func (a *Auth) IsAdmin(ctx context.Context, userID int64) (bool, error) {
+	const op = "auth.IsAdmin"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.Int64("user id", userID), //careful with sensitive data!
+	)
+	log.Info("checking is user is admin")
+
+	isAdmin, err := a.userProvider.IsAdmin(ctx, userID)
+	if err != nil {
+		if errors.Is(err, storage.ErrAppNotFound) {
+			a.log.Warn("user not found", err.Error())
+			return false, fmt.Errorf("%s : %w", op, ErrInvalidAppId)
+		}
+
+		return false, fmt.Errorf("%s : %w", op, err)
+	}
+
+	log.Info("checked is user is admin", slog.Bool("is_admin", isAdmin))
+
+	return isAdmin, nil
+}
